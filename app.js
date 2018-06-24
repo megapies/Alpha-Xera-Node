1d275353eee84be19bf48ab30ae8110a786d98ee
natthapach <natthapach.a@gmail.com>
2018-06-24 23:47:10 +0700

    hello mobile

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json())
 app.post('/webhook', function (req, res) {
   console.log(req.body.events)
   console.log('hello github')
+  console.log('hello mobile')
   const event = req.body.events[0]
   const event_type = event.type
   console.log(req.body);
