464657dcee8886659ebf1e25a6f10ed9105043d3
natthapach <natthapach.a@ku.th>
2018-06-30 23:39:50 +0700

    add leaflet commander

---
+++ commanders/leaflet.js
@@ -0,0 +1,25 @@
+const Replyer = require('../helpers/replyer')
+const command = /ขอใบลา/
+const anti_command = /ลาออก/
+class LeafletCommander{
+  
+  name(){
+    return 'Leaflet'
+  }
+  checkMessage(msg){
+    return command.test(msg) && !anti_command.test(msg)
+  }
+
+  async perform(event){
+    const replyToken = event.replyToken
+    // const food = 'ผัดกระเพรา'
+    const msg = `นี้ค่ะใบลา
+    http://ms.src.ku.ac.th/manual/document/ใบลา.pdf`
+    await Replyer.replyMessage(replyToken, msg)
+  }
+}
+
+module.exports = {
+  staticClass: LeafletCommander,
+  commander: new LeafletCommander(),
+}
