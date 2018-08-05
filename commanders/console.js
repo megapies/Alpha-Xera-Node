1a1086b3f5c4977b2aaf52a9bedfe06a30efa4ff
natthapach <natthapach.a@ku.th>
2018-08-05 15:49:48 +0800

    implement tokenize

--- commanders/console.js
+++ commanders/console.js
@@ -14,7 +14,44 @@ class FightingCommander{
     const replyToken = event.replyToken
     // const food = 'ผัดกระเพรา'
     const msg = `xera operation complete`
+    const token = this.tokenize(event.message)
+    console.log(token)
     await Replyer.replyMessage(replyToken, msg)
+  }
+
+  tokenize(message){
+    message = message.slice(5, message.length)
+    const token = {
+      command: null,
+      flags: [
+
+      ],
+      params: [
+
+      ]
+    }
+    let buffer = ''
+    let inQuote = false
+    let quote = ''
+    for(let c of message){
+      if(c === ' ' && !inQuote){
+        if(token.command === null)
+          token.command = buffer
+        else if(buffer[0] === '-')
+          token.flags.push(buffer)
+        else 
+          tokem.params.push(buffer)
+      }
+      else if((c === '\"' || c === '\'') && c !== quote){
+        inQuote = !inQuote
+        quote = (quote === '')?c:''
+      }
+      else {
+        buffer = buffer + c
+      }
+    }
+
+    return token
   }
 }
 
