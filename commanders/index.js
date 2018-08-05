97952fbd226171242e4fa613470f37b240c1ed2c
natthapach <natthapach.a@ku.th>
2018-08-05 21:56:33 +0800

    comander include only .js

--- commanders/index.js
+++ commanders/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const commanders = [];
 fs.readdirSync(__dirname)
-  .filter(file => (file != 'index.js'))
+  .filter(file => (file != 'index.js' && /.js$/.test(file)))
   .forEach((file) => {
     console.log('regis commander', file)
     const { commander } = require('./' + file)
