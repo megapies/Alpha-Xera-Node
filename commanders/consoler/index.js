a715907700dcde7b5f83d6f34db4dc41bc916560
natthapach <natthapach.a@ku.th>
2018-08-05 21:24:59 +0800

    fix bug

--- commanders/consoler/index.js
+++ commanders/consoler/index.js
@@ -1,14 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 
-const consoler = [];
+const consolers = [];
 fs.readdirSync(__dirname)
   .filter(file => (file != 'index.js'))
   .forEach((file) => {
     console.log('regis consoler', file)
     const { consoler } = require('./' + file)
     console.log(consoler)
-    consoler.push(consoler)
+    consolers.push(consoler)
   })
 
-module.exports = consoler
+module.exports = consolers
